79e199f7b4c8f2688573e2127a97d1447752bc19
Kenny <144939416+NULLK3NNY@users.noreply.github.com>
2026-07-18 20:06:23 +0100

    s

--- LU/Game/Entity.h
+++ LU/Game/Entity.h
@@ -9,6 +9,7 @@ public:
 	void SetPositionV(Vector2 position);
 	void SetPositionF(float x, float y);
 	void SetTexture(Texture texture);
+	void Move();
 protected:
 	Vector2 position = { 0, 0 };
 	Texture2D texture = {};
